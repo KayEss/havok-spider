e71a9af12164b8e8a0e2cf8423f30419fb5c6b0b
Kirit Sælensminde <k@kirit.com>
2008-10-20 12:47:52 +0000

    SHA1 now works using the fostlib::string class, but there is no way yet to convert it transparently into a Python string.

--- Cpp/fost-crypto-py/crypto.cpp
+++ Cpp/fost-crypto-py/crypto.cpp
@@ -6,19 +6,19 @@
 */
 
 
-#include <boost/python/module.hpp>
-#include <boost/python/def.hpp>
+#include <boost/python.hpp>
 #include <fost/crypto>
 
 
-namespace {
-    std::wstring sha1( const std::wstring &str ) {
-        return L"2fd4e1c67a2d28fced849ee1bb76e7391b93eb12";
-        //return fostlib::sha1( fostlib::string( str.c_str() ) ).std_str();
-    }
-}
-
 BOOST_PYTHON_MODULE( crypto ) {
     using namespace boost::python;
-    def("sha1", sha1);
+
+    class_< fostlib::string >( "foststring", init< fostlib::native_string >() )
+            .def( "length", &fostlib::string::length )
+            .def( "u", &fostlib::string::std_str )
+        ;
+
+    def( "sha1", fostlib::sha1 );
+
+    implicitly_convertible< fostlib::native_string, fostlib::string >();
 }
