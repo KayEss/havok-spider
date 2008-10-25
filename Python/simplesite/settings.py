9adef181401d0ad7c663636194d74840c877fca8
Kirit Sælensminde <k@kirit.com>
2008-10-25 12:46:12 +0000

    Converted the default Django settings to use Fost.settings.

--- Python/simplesite/settings.py
+++ Python/simplesite/settings.py
@@ -1,79 +1,32 @@
-# Django settings for simplesite project.
+from Fost.settings import database
+import os
+from socket import gethostname
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
+settingsdb = database()
+basepath = os.path.split(__import__(os.environ.get("DJANGO_SETTINGS_MODULE", "settings")).__file__)[0]
+settingsdb.file(os.path.join(basepath, "settings.ini"))
+settingsdb.file(os.path.join(basepath, "settings-%s.ini" % gethostname()))
 
-ADMINS = (
-    # ('Your Name', 'your_email@domain.com'),
-)
-
-MANAGERS = ADMINS
-
-DATABASE_ENGINE = ''           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = ''             # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
-
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
-TIME_ZONE = 'America/Chicago'
-
-# Language code for this installation. All choices can be found here:
-# http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-us'
-
-SITE_ID = 1
-
-# If you set this to False, Django will make some optimizations so as not
-# to load the internationalization machinery.
-USE_I18N = True
-
-# Absolute path to the directory that holds media.
-# Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ''
-
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash if there is a path component (optional in other cases).
-# Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = ''
-
-# URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
-# trailing slash.
-# Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
-
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = '5!9ra0sl0ujdenng-iobjf4cdq+vc)c9(9+t_h-%-be@up_0a7'
-
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.load_template_source',
-    'django.template.loaders.app_directories.load_template_source',
-#     'django.template.loaders.eggs.load_template_source',
-)
-
-MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-)
-
-ROOT_URLCONF = 'simplesite.urls'
-
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-)
-
-INSTALLED_APPS = (
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-)
+DEBUG = settingsdb["Django", "DEBUG"]
+TEMPLATE_DEBUG = settingsdb["Django", "TEMPLATE_DEBUG"]
+ADMINS =settingsdb["Django", "ADMINS"]
+MANAGERS = settingsdb["Django", "MANAGERS"]
+DATABASE_ENGINE = settingsdb["Django", "DATABASE_ENGINE"] or ''
+DATABASE_NAME = settingsdb["Django", "DATABASE_NAME"] or ''
+DATABASE_USER = settingsdb["Django", "DATABASE_USER"] or ''
+DATABASE_PASSWORD = settingsdb["Django", "DATABASE_PASSWORD"] or ''
+DATABASE_HOST = settingsdb["Django", "DATABASE_HOST"] or ''
+DATABASE_PORT = settingsdb["Django", "DATABASE_PORT"] or ''
+TIME_ZONE = settingsdb["Django", "TIME_ZONE"]
+LANGUAGE_CODE = settingsdb["Django", "LANGUAGE_CODE"]
+SITE_ID = settingsdb["Django", "SITE_ID"]
+USE_I18N = settingsdb["Django", "USE_I18N"]
+MEDIA_ROOT = settingsdb["Django", "MEDIA_ROOT"] or ''
+MEDIA_URL = settingsdb["Django", "MEDIA_URL"] or ''
+ADMIN_MEDIA_PREFIX = settingsdb["Django", "ADMIN_MEDIA_PREFIX"]
+SECRET_KEY = settingsdb["Django", "SECRET_KEY"]
+TEMPLATE_LOADERS = settingsdb["Django", "TEMPLATE_LOADERS"]
+MIDDLEWARE_CLASSES =settingsdb["Django", "MIDDLEWARE_CLASSES"]
+ROOT_URLCONF = settingsdb["Django", "ROOT_URLCONF"]
+TEMPLATE_DIRS = settingsdb["Django", "TEMPLATE_DIRS"]
+INSTALLED_APPS = [str(a) for a in settingsdb["Django", "INSTALLED_APPS"]]
