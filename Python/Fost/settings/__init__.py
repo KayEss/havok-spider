8edc59d11c0d271b82ccf41fcb55ecff945cba51
Kirit Sælensminde <k@kirit.com>
2008-10-25 10:51:36 +0000

    Added copyright notices and removed settings code from Fost's __init__.

--- Python/Fost/settings/__init__.py
+++ Python/Fost/settings/__init__.py
@@ -1,3 +1,7 @@
+# Copyright 2008, Felspar Co Ltd. http://fost.3.felspar.com/
+# Distributed under the Boost Software License, Version 1.0.
+# See accompanying file LICENSE_1_0.txt or copy at
+#     http://www.boost.org/LICENSE_1_0.txt
 import _settings
 
 class database(_settings.settings):
