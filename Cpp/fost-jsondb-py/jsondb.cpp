64cd732129a08915daefbdfa3b2b56b76131da4a
Kirit Sælensminde <k@kirit.com>
2008-12-23 06:54:29 +0000

    Added in the basic code required for the Json DB wrapper for Python.

---
+++ Cpp/fost-jsondb-py/jsondb.cpp
@@ -0,0 +1,21 @@
+/*
+    Copyright 2008, Felspar Co Ltd. http://fost.3.felspar.com/
+    Distributed under the Boost Software License, Version 1.0.
+    See accompanying file LICENSE_1_0.txt or copy at
+        http://www.boost.org/LICENSE_1_0.txt
+*/
+
+
+#include <fost/pybind.hpp>
+
+
+BOOST_PYTHON_MODULE( _jsondb ) {
+    using namespace boost::python;
+
+    implicitly_convertible< fostlib::native_string, fostlib::string >();
+    boost::python::converter::registry::push_back(
+        fostlib::from_pystr::convertible, fostlib::from_pystr::construct,
+        boost::python::type_id< fostlib::string >()
+    );
+    to_python_converter< fostlib::string, fostlib::to_pystr, false >();
+}
