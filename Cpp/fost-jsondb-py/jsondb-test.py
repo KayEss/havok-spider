b8ba57a0eb3235c3320580bd1997ca449707931d
Kirit Sælensminde <k@kirit.com>
2008-12-29 12:50:45 +0000

    We can now initialise the JSON database with a Python object.

--- Cpp/fost-jsondb-py/jsondb-test.py
+++ Cpp/fost-jsondb-py/jsondb-test.py
@@ -4,11 +4,11 @@ blob1 = _jsondb.jsonblob()
 assert blob1.filename == None, "filename: '%s'" % blob1.filename
 blob2 = _jsondb.jsonblob("Cpp/fost-jsondb-py/testfile.json")
 assert blob2.filename == "Cpp/fost-jsondb-py/testfile.json", "filename: '%s'" % blob2.filename
-#blob3 = _jsondb.jsonblob("Cpp/fost-jsondb-py/testfile.json", dict(
-#    hello = 'nightclub',
-#    goodbye = 'country'
-#))
-#assert blob3.filename == "Cpp/fost-jsondb-py/testfile.json", "filename: '%s'" % blob3.filename
+blob3 = _jsondb.jsonblob("Cpp/fost-jsondb-py/testfile.json", dict(
+    hello = 'nightclub',
+    goodbye = 'country'
+))
+assert blob3.filename == "Cpp/fost-jsondb-py/testfile.json", "filename: '%s'" % blob3.filename
 
 blob4 = blob2
 assert blob4.filename == "Cpp/fost-jsondb-py/testfile.json", "filename: '%s'" % blob4.filename
