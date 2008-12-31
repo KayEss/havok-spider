53b7f9e3b25d1dda98beb60135b53b59d50d3128
Kirit Sælensminde <k@kirit.com>
2008-12-31 10:11:29 +0000

    Now tidies up the file that is created.

--- Cpp/fost-jsondb-py/jsondb-test.py
+++ Cpp/fost-jsondb-py/jsondb-test.py
@@ -1,9 +1,16 @@
-import _jsondb
+import _jsondb, os
+
+def rmfile(f):
+    if os.path.isfile(f):
+        os.remove(f)
+
 
 blob1 = _jsondb.jsonblob()
 assert blob1.filename == None, "filename: '%s'" % blob1.filename
 blob2 = _jsondb.jsonblob("Cpp/fost-jsondb-py/testfile.json")
 assert blob2.filename == "Cpp/fost-jsondb-py/testfile.json", "filename: '%s'" % blob2.filename
+
+rmfile("Cpp/fost-jsondb-py/testfile-new.json")
 blob3 = _jsondb.jsonblob("Cpp/fost-jsondb-py/testfile-new.json", dict(
     hello = 'nightclub',
     goodbye = 'country',
@@ -14,3 +21,6 @@ assert blob4.filename == "Cpp/fost-jsondb-py/testfile.json", "filename: '%s'" % 
 
 local3 = blob3.local
 assert local3 == dict(hello = 'nightclub', goodbye = 'country'), local3.json
+
+
+rmfile("Cpp/fost-jsondb-py/testfile-new.json")
