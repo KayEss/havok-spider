0259e4cfbc00bc8731568d44a90a4a8396f24e40
Kirit Sælensminde <k@kirit.com>
2009-01-10 05:02:27 +0000

    Added a basic wrapper for adding to the jcursor path.

--- Cpp/fost-json-py/jcursor-test.py
+++ Cpp/fost-json-py/jcursor-test.py
@@ -1,5 +1,12 @@
+# Copyright 2009, Felspar Co Ltd. http://fost.3.felspar.com/
+# Distributed under the Boost Software License, Version 1.0.
+# See accompanying file LICENSE_1_0.txt or copy at
+#     http://www.boost.org/LICENSE_1_0.txt
+
 import _json
 
 # Basic constructor
 j1 = _json.jcursor()
 
+# Add to the path of the jcursor
+j1 /= "path"
