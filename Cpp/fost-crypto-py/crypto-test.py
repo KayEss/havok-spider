43bd8c13d7ee5029cb07b489e4a2e855670e5a7b
Kirit Sælensminde <k@kirit.com>
2009-01-24 06:56:18 +0000

    SHA1 HMAC now works from Python. This can now be used to build the REST authentication HMAC signature that comes from FOST.3's HTTP client.

--- Cpp/fost-crypto-py/crypto-test.py
+++ Cpp/fost-crypto-py/crypto-test.py
@@ -17,3 +17,12 @@ assert _crypto.sha1("The quick brown fox jumps over the lazy dog") \
 assert _crypto.sha1(u"The quick brown fox jumps over the lazy dog") \
     == "2fd4e1c67a2d28fced849ee1bb76e7391b93eb12", \
     _crypto.sha1(u"The quick brown fox jumps over the lazy dog")
+
+assert _crypto.sha1_hmac(
+        "uV3F3YluFJax1cknvbcGwgjvx4QpvB+leU8dUj2o",
+        "GET\n\n\nTue, 27 Mar 2007 19:36:42 +0000\n/johnsmith/photos/puppy.jpg"
+    ) == "xXjDGYUmKxnwqr5KXNPGldn5LbA=", \
+    _crypto.sha1_hmac(
+        "uV3F3YluFJax1cknvbcGwgjvx4QpvB+leU8dUj2o",
+        "GET\n\n\nTue, 27 Mar 2007 19:36:42 +0000\n/johnsmith/photos/puppy.jpg"
+    )
