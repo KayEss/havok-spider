fa6143dc05f97be94b270767e2bc1265664acceb
Kirit Sælensminde <k@kirit.com>
2009-03-03 07:52:57 +0000

    Added another couple of tests for the unparser.

--- Cpp/fost-json-py/basic-test.py
+++ Cpp/fost-json-py/basic-test.py
@@ -5,5 +5,13 @@
 
 import _json
 
-assert _json.parse("{}") == dict()
-assert _json.unparse(_json.parse("{}"), False) == "{}"
+assert _json.parse("{}") == dict(), _json.parse("{}")
+assert _json.unparse(_json.parse("{}"), False) == "{}", _json.unparse(_json.parse("{}"), False)
+assert _json.unparse(_json.parse("{}"), True) == "{\n}\n", _json.unparse(_json.parse("{}"), True)
+
+assert _json.unparse(None, False) == "null", _json.unparse(None, False)
+assert _json.unparse(12, False) == "12", _json.unparse(12, False)
+assert _json.unparse({}, False) == "{}", _json.unparse({}, False)
+assert _json.unparse(dict(), False) == "{}", _json.unparse({}, False)
+assert _json.unparse([12], False) == "[12]", _json.unparse([12], False)
+
