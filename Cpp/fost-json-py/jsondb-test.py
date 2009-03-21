c841796aae1999cae1fb063d4e67a13550140c49
Kirit Sælensminde <k@kirit.com>
2009-03-21 05:33:29 +0000

    Added some tests for the mutators added to the JSON blob database.

--- Cpp/fost-json-py/jsondb-test.py
+++ Cpp/fost-json-py/jsondb-test.py
@@ -25,6 +25,29 @@ assert blob4.filename == "Cpp/fost-json-py/testfile.json", "filename: '%s'" % bl
 local3 = blob3.local
 assert local3 == dict(hello = 'nightclub', goodbye = 'country'), local3.json
 
+# Grab two locals, update 1 and check that is not visible in the other then do a commit and check again
+local1 = blob3.local
+local2 = blob3.local
+local1[_json.jcursor()/'hello'] = 'goodbye'
+assert local1 == dict(hello = 'goodbye', goodbye = 'country'), local1
+assert local2 == dict(hello = 'nightclub', goodbye = 'country'), local2
+local1.commit()
+assert local1 == dict(hello = 'goodbye', goodbye = 'country'), local1
+# This is right because local2's transaction started before the commit
+assert local2 == dict(hello = 'nightclub', goodbye = 'country'), local2
+
+# This time grab the second local after the update has started on the first
+# This indrectly tests that the earlier transaction becomes visible to later ones
+local1 = blob3.local
+local1[_json.jcursor()/'hello'] = 'goodbye 2'
+local2 = blob3.local
+assert local1 == dict(hello = 'goodbye 2', goodbye = 'country'), local1
+assert local2 == dict(hello = 'goodbye', goodbye = 'country'), local2
+local1.commit()
+assert local1 == dict(hello = 'goodbye 2', goodbye = 'country'), local1
+# This is still right because local2's transaction started before the commit
+assert local2 == dict(hello = 'goodbye', goodbye = 'country'), local2
+
 
 # Tidy up files
 rmfile("Cpp/fost-json-py/testfile-new.json")
