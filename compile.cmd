d5b142da6190fd1d3444bf9bd44385d94375667c
Kirit Sælensminde <k@kirit.com>
2009-05-13 12:10:48 +0000

    The PDB files are now only copied from the current build folder.

--- compile.cmd
+++ compile.cmd
@@ -1,5 +1,6 @@
 ..\bjam toolset=msvc preserve-test-targets=on %*
 @echo off
+call ..\boost-version.cmd
 IF NOT ERRORLEVEL 1 (
-    for /r ..\build.tmp %%f in (*.pdb) do xcopy /D /Y %%f ..\dist\bin
+    for /r %BUILD_DIRECTORY% %%f in (*.pdb) do xcopy /D /Y %%f ..\dist\bin
 )
