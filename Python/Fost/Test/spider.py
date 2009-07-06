fda38ba5c2d167d85284dbccd82f826e34617de6
Kirit Sælensminde <k@kirit.com>
2009-07-06 17:02:19 +0000

    This version of the spider can be used to test Django applications.

--- Python/Fost/Test/spider.py
+++ Python/Fost/Test/spider.py
@@ -1,17 +1,40 @@
 # -*- coding: utf-8 -*-
 import cookielib, glob, os, random, unittest, urllib, urllib2, urlparse
-from BeautifulSoup import BeautifulSoup, HTMLParseError
+from BeautifulSoup import BeautifulSoup
+from Fost.internet import x_www_form_urlencoded
 from Fost.internet.useragent import agent
 
 import Fost.settings
 fostsettings = Fost.settings.database()
 
+def queue_links(spider, response):
+    soup = response.soup
+    # Check links in a random order
+    links = soup.findAll('a')
+    random.shuffle(links)
+    for link in links:
+        if link.has_key('href') and not link['href'].startswith('http') and not link['href'].startswith('/__'):
+            spider.spider_test(urlparse.urljoin(response.url, link['href']))
+    return soup
+def ignore_links(spider, response):
+    return response.soup
+
+def print_error(test, fetch, e, url, data):
+    if data:
+        test.assert_(False, u"HTTP error with POST against %s with data\n%s\nBase URL %s\n%s" % (fetch, e, url, data))
+    elif data == "":
+        test.assert_(False, u"HTTP error with POST against %s with empty data\n%s\nBase URL %s" % (fetch, e, url))
+    else:
+        test.assert_(False, u"HTTP error with GET against %s\n%s\nBase URL %s" % (fetch, e, url))
+
+
 class Spider(object):
-    def __init__(self, urls, visited):
+    def __init__(self, urls = [], visited = {}, links = queue_links):
         self.agent = agent()
         self.suite = unittest.TestSuite()
         self.pages = dict([(urlparse.urljoin(fostsettings["Spider", "host"], url), v) for url, v in visited.items()])
         [self.spider_test(urlparse.urljoin(fostsettings["Spider", "host"], url)) for url in urls]
+        self.links = queue_links
 
     def url_data(self, url):
         if url.find('?') > 0:
@@ -25,63 +48,69 @@ class Spider(object):
     def run_suite(self):
         unittest.TextTestRunner().run(self.suite)
 
-    def spider_test(spider, url, data=None):
-        if spider.suite.countTestCases() < fostsettings["Spider", "Count"] and spider.url_data(url)['remaining']:
-            if spider.url_data(url)['remaining']: spider.url_data(url)['remaining'] -= 1
-            class Test(unittest.TestCase):
-                def fetch(self, fetch, data = None):
-                    try:
-                        def error_display(m, e):
-                            if data:
-                                self.assert_(False, u"HTTP error with POST against %s with data\n%s\nBase URL %s\n%s" % (fetch, e, url, data))
-                            elif data == "":
-                                self.assert_(False, u"HTTP error with POST against %s with empty data\n%s\nBase URL %s" % (fetch, e, url))
-                            else:
-                                self.assert_(False, u"HTTP error with GET against %s\n%s\nBase URL %s" % (fetch, e, url))
-                        return spider.agent.process(fetch, spider.url_data(fetch), data)
-                    except urllib2.HTTPError, e:
-                        if int(str(e).split()[2][0:3]) == spider.url_data(fetch).get('status', 200):
-                            # This is OK -- the status matches what we're expecting
-                            class response(object):
-                                soup = BeautifulSoup('')
-                                url = fetch
-                            return response()
+    def addTest(spider, url, data=None):
+        if url[0]=='/':
+            url = urlparse.urljoin(fostsettings["Spider", "host"], url)
+            if not spider.pages.has_key(url):
+                spider.pages[url] = dict()
+            if not spider.pages[url].has_key('remaining'):
+                spider.pages[url]['remaining'] = 1
+        class Test(unittest.TestCase):
+            def __init__(self, *args, **kwargs):
+                super(Test, self).__init__(*args, **kwargs)
+                self.links = queue_links
+            def fetch(self, fetch, data = None):
+                try:
+                    response = spider.agent.fetch(fetch, data)
+                    if response.url != url and spider.url_data(url)['remaining']:
+                        spider.url_data(url)['remaining'] -= 1
+                    if response.headers['Content-Type'].split(';')[0] == 'text/html':
+                        response.soup = BeautifulSoup(response.read())
+                    else:
+                        response.soup = BeautifulSoup('')
+                    return response
+                except urllib2.HTTPError, e:
+                    if int(str(e).split()[2][0:3]) in spider.pages[fetch].get('status', [200, 301, 302, 303]):
+                        # This is OK -- the status matches what we're expecting
+                        class response(object):
+                            soup = BeautifulSoup('')
+                            url = fetch
+                        return response()
+                    else:
+                        print_error(self, fetch, e, url, data)
+                except Exception, e:
+                    print_error(self, fetch, e, url, data)
+            def process(self, response):
+                soup = spider.links(spider, response)
+                # Look for forms to submit
+                if spider.url_data(response.url).get('use_forms', True):
+                    for form in soup.findAll('form'):
+                        spider_url_data = spider.url_data(response.url)
+                        form_id = form.get('id', form.get('name', None))
+                        if form_id and spider_url_data.has_key('forms') and spider_url_data['forms'].has_key(form_id):
+                            form_data = spider_url_data['forms'][form_id]
                         else:
-                            error_display("HTTP error", unicode(e))
-                    except HTMLParseError, e:
-                        error_display("HTML parse error", unicode(e))
-                def queue_links(self, response):
-                    soup = response.soup
-                    # Check links in a random order
-                    links = soup.findAll('a')
-                    random.shuffle(links)
-                    for link in links:
-                        if link.has_key('href') and not link['href'].startswith('http') and not link['href'].startswith('/__'):
-                            spider.spider_test(urlparse.urljoin(response.url, link['href']))
-                    return soup
-                def process(self, response):
-                    soup = self.queue_links(response)
-                    # Look for forms to submit
-                    if spider.url_data(response.url).get('use_forms', True):
-                        for form in soup.findAll('form'):
-                            spider_url_data = spider.url_data(response.url)
-                            form_id = form.get('id', form.get('name', None))
-                            if form_id and spider_url_data.has_key('forms') and spider_url_data['forms'].has_key(form_id):
-                                form_data = spider_url_data['forms'][form_id]
+                            form_data = spider_url_data
+                        submit, query = build_form_query(self, form, response.url)
+                        if form_data.has_key('data'):
+                            for k, v in form_data['data'].items():
+                                query[k] = v
+                        if submit:
+                            if form.get('method', 'get').lower() == 'get':
+                                spider.spider_test(urlparse.urljoin(response.url, u'%s?%s' % (form['action'], x_www_form_urlencoded(query))))
                             else:
-                                form_data = spider_url_data
-                            submit, query = build_form_query(self, form, response.url)
-                            if form_data.has_key('data'):
-                                for k, v in form_data['data'].items():
-                                    query[k] = v
-                            if submit:
-                                if form.get('method', 'get').lower() == 'get':
-                                    spider.spider_test(urlparse.urljoin(response.url, u'%s?%s' % (form['action'], urllib.urlencode(query))))
-                                else:
-                                    self.queue_links(self.fetch(urlparse.urljoin(response.url, form['action']), urllib.urlencode(query)))
-                def runTest(self):
-                    self.process(self.fetch(url, data))
-            spider.suite.addTest(Test())
+                                self.links(spider, self.fetch(urlparse.urljoin(response.url, form['action']), x_www_form_urlencoded(query)))
+            def runTest(self):
+                self.process(self.fetch(url, data))
+        spider.suite.addTest(Test())
+
+    def spider_test(spider, url, data=None):
+        if spider.suite.countTestCases() < fostsettings["Spider", "Count"]:
+            if spider.url_data(url)['remaining']:
+                spider.url_data(url)['remaining'] -= 1
+                spider.addTest(url, data)
+
+spider_instance = Spider()
 
 
 def test_response(test, response):
@@ -134,7 +163,10 @@ def build_form_query(test, form, base_url, form_data = {}, submit_button = None)
         test.assert_(len(options), u"No options found in select at %s" % base_url)
         for option in options:
             test.assert_(option.has_key('value'), u'No value found for option %s in select at %s' % (option, base_url))
-            query[select['name']] = option['value']
+            if option.get('selected', None):
+                query[select['name']] = option['value']
+        if not query.has_key(select['name']) and len(options):
+            query[select['name']] = options[0]['value']
     if len(submits):
         button = submits[random.randint(0, len(submits) - 1)]
         if button.has_key('name'):
