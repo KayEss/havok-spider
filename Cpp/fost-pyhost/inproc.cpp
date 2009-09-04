76642009a35bde0fcf5349b134584b9e1b66c12a
Kirit Sælensminde <k@kirit.com>
2009-09-04 13:07:29 +0000

    Refactored the host to simplify some of the logic and so that we can add some new constructors later on.

--- Cpp/fost-pyhost/inproc.cpp
+++ Cpp/fost-pyhost/inproc.cpp
@@ -17,19 +17,17 @@ namespace {
         static boost::mutex m;
         return m;
     }
-    bool g_already_running = false;
-
-    boost::python::object g_main_module;
-    boost::python::dict g_main_namespace;
+    struct host {
+        host();
+        ~host();
+        boost::python::object main_module;
+        boost::python::dict main_namespace;
+    };
+    boost::scoped_ptr< host > g_host;
 }
 
 
-fostlib::python::inproc_host::inproc_host() {
-    boost::mutex::scoped_lock lock(g_mutex());
-    if ( g_already_running )
-        throw exceptions::not_implemented("Error handling for creating more than one inproc_host");
-    g_already_running = true;
-
+host::host() {
     Py_Initialize();
 
     try {
@@ -37,29 +35,42 @@ fostlib::python::inproc_host::inproc_host() {
         fostlib::python_json_registration();
 
         // We need these two to provide context for the scripts
-        g_main_module = boost::python::import( "__main__" );
-        g_main_namespace = boost::python::dict(g_main_module.attr( "__dict__" ));
+        main_module = boost::python::import( "__main__" );
+        main_namespace = boost::python::dict(main_module.attr( "__dict__" ));
     } catch ( ... ) {
-        this->~inproc_host();
+        Py_Finalize();
         throw;
     }
 }
-
-fostlib::python::inproc_host::~inproc_host() {
+host::~host() {
     Py_Finalize();
-    g_already_running = false;
 }
 
 
-void fostlib::python::inproc_host::operator () ( const boost::filesystem::wpath &f, boost::python::list args, boost::python::dict kwargs ) {
+fostlib::python::inproc_host::inproc_host() {
+    boost::mutex::scoped_lock lock(g_mutex());
+    if ( g_host.get() )
+        throw exceptions::not_implemented("Error handling for creating more than one inproc_host");
+    g_host.reset( new host );
+}
+
+fostlib::python::inproc_host::~inproc_host() {
+    g_host.reset(NULL);
+}
+
+
+void fostlib::python::inproc_host::operator () (
+    const boost::filesystem::wpath &f,
+    boost::python::list args, boost::python::dict kwargs
+) {
     boost::python::exec_file(
         fostlib::coerce< fostlib::utf8string >( fostlib::coerce< fostlib::string >( f.string() ) ).c_str(),
-        g_main_namespace, g_main_namespace
+        g_host->main_namespace, g_host->main_namespace
     );
 
     // Find main and call it through a lambda to handle the arguments for us
-    if ( !g_main_namespace.has_key( "main" ) )
+    if ( !g_host->main_namespace.has_key( "main" ) )
         throw fostlib::exceptions::null( L"No main() in the loaded Python file" );
-    boost::python::object main_func = g_main_namespace[ "main" ];
+    boost::python::object main_func = g_host->main_namespace[ "main" ];
     boost::python::eval( "lambda f, a, k: f(*a, **k)" )( main_func, args, kwargs );
 }
