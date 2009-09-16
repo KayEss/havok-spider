863800c53c43a4e8abcd3f3943d1c13bf58cfa34
Kirit Sælensminde <k@kirit.com>
2009-09-16 12:11:58 +0000

    * fostlib::const_memory_block can now be coerced to fostlib::utf8string. * The new fostlib::workerpool now works properly on Windows. * Tagged strings can now be type promoted from const literals. The literals are assumed to be pre-encoded. * Added a simple SMTP client for sending emails. * URL file specifications can now be encoded from ASCII strings. * Emails are now always turned into strings with the RFC822 part enclosed in angled brackets. * Moved the fost-internet examples around and added a multi-threaded web server example. * Made some changes to the WSGI server example to prepare it for multi-threading.

--- Examples/wsgi/wsgi.cpp
+++ Examples/wsgi/wsgi.cpp
@@ -8,10 +8,10 @@
 
 #include <fost/pyhost>
 #include <fost/wsgi>
-#include <fost/cli>
 #include <fost/main>
 #include <fost/internet>
 #include <fost/detail/http.server.hpp>
+#include <fost/threading>
 
 
 using namespace fostlib;
@@ -22,6 +22,11 @@ namespace {
     setting< int > c_port( L"fwsgi", L"Server", L"Port", 8001, true );
 
     setting< string > c_application( L"fwsgi", L"WSGI", L"Application", "wsgiref.simple_server.demo_app", true );
+
+    bool service( python::wsgi::application &app, boost::shared_ptr< http::server::request > req ) {
+        (*req)( *app(*req) );
+        return true;
+    }
 }
 
 
@@ -29,20 +34,22 @@ FSL_MAIN(
     L"fwsgi",
     L"Simple HTTP WSGI server\nCopyright (c) 2008-2009, Felspar Co. Ltd."
 )( fostlib::ostream &o, fostlib::arguments &args ) {
+    // Create a worker pool to service the requests
+    workerpool pool;
     // Work out the IP and port bindings
     http::server server( host( args[1].value(c_host.value()) ), c_port.value() );
     o << L"Answering requests on http://" << server.binding() << L":" << server.port() << L"/" << std::endl;
     // Create the Python host
     fostlib::python::inproc_host host;
-
     // Build the application object that will handle the web service
     python::wsgi::application app( c_application.value() );
-
     // Keep serving forever
     for ( bool process( true ); process; ) {
-        std::auto_ptr< http::server::request > req( server() );
-        o << req->method() << L" " << req->file_spec() << std::endl;
-        (*req)( *app(*req) );
+        boost::shared_ptr< http::server::request > req( server().release() );
+        o << req->method() << L" " << req->file_spec() << " peak threads: " << pool.peak_used() << std::endl;
+        // Threaded version breaks in the Python libraries when under siege
+        //pool.f<bool>( boost::lambda::bind( service, boost::ref(app), req ) );
+        service(app, req);
     }
     return 0;
 }
