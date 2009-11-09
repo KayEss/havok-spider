f1a6b9113090716170f3c06d492445f5e4198294
Kirit Sælensminde <k@kirit.com>
2009-11-09 05:55:12 +0000

    * The old fostlib::utf8string (a typedef to std::string) is now replaced by a proper tagged string implementation, fostlib::utf8_string. This makes the use of UTF8 strings safer and the encoding/decoding of the strings explicit. * A fix to the spider so that it can properly handle non-200 status codes when running tests.

--- Cpp/include/fost/detail/pybind.hpp
+++ Cpp/include/fost/detail/pybind.hpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 2008, Felspar Co Ltd. http://fost.3.felspar.com/
+    Copyright 2008-2009, Felspar Co Ltd. http://fost.3.felspar.com/
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
@@ -17,26 +17,20 @@
 
 namespace fostlib {
 
-    /*
-        Call this inside the BOOST_PYTHON_MODULE function to enable fostlib::string handling
-    */
+    ///Call this inside the BOOST_PYTHON_MODULE function to enable fostlib::string handling
     FOST_PYTHON_DECLSPEC void python_string_registration();
 
     template<>
     struct coercer< boost::python::str, string > {
         boost::python::str coerce( const string &s ) {
-            return fostlib::coerce< utf8string >( s ).c_str();
+            return fostlib::coerce< utf8_string >( s ).underlying().c_str();
         }
     };
 
-    /*
-        Call this inside the BOOST_PYTHON_MODULE function to enable fostlib::json handling
-    */
+    /// Call this inside the BOOST_PYTHON_MODULE function to enable fostlib::json handling
     FOST_PYTHON_DECLSPEC void python_json_registration();
 
-    /*
-        Allow meta accessors to be mapped to properties or methods
-    */
+    /// Allow meta accessors to be mapped to properties or methods
     template< typename O, typename V, accessors< V > O::*a >
     V accessors_getter(const O *o) {
         return (o->*a)();
