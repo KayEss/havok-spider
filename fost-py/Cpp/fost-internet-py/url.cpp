be2a6c352e8298d15d1337f10e4905268262dd88
Kirit Sælensminde <k@kirit.com>
2010-03-29 08:36:45 +0000

    A Python string (containing byte data) is now provided back by the user agent rather than a Unicode object.

--- fost-py/Cpp/fost-internet-py/url.cpp
+++ fost-py/Cpp/fost-internet-py/url.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 2009, Felspar Co Ltd. http://fost.3.felspar.com/
+    Copyright 2009-2010, Felspar Co Ltd. http://fost.3.felspar.com/
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
@@ -42,18 +42,23 @@ url url_join(const url &u, const string &r) {
 }
 
 
-void ua_fost_authenticate(http::user_agent &ua, const string &key, const string &secret) {
-    ua.authentication(boost::function< void ( fostlib::http::user_agent::request& ) >(boost::lambda::bind(
+void ua_fost_authenticate(
+    http::user_agent &ua, const string &key, const string &secret
+) {
+    ua.authentication(boost::function<
+        void ( fostlib::http::user_agent::request& )
+    >(boost::lambda::bind(
         fostlib::http::fost_authentication, key, secret, std::set< fostlib::string >(), boost::lambda::_1
     )));
 }
 
-string ua_response_body(const http::user_agent::response &response) {
-    const mime &body = response.body();
-    if ( dynamic_cast< const text_body * >( &body ) )
-        return coerce< string >(dynamic_cast< const text_body & >( body ).text());
-    else if ( dynamic_cast< const empty_mime * >( &body ) )
-        return string();
-    else
-        throw exceptions::not_implemented("ua_response_body(const http::user_agent::response &response) for non text responses");
+std::string ua_response_body(http::user_agent::response &response) {
+    std::string ret;
+    const binary_body &body = response.body();
+    for ( mime::const_iterator part( body.begin() ); part != body.end(); ++part )
+        ret += std::string(
+            reinterpret_cast< nliteral >((*part).first),
+            reinterpret_cast< nliteral >((*part).second)
+        );
+    return ret;
 }
