13eeefc8c7183b1cf1ab401996b851de736c683e
Kirit Sælensminde <k@kirit.com>
2011-04-11 02:53:42 +0000

    Try to use unittest2 if it's available, otherwise fall back to unittest.

--- fost-py/Python/Fost/Test/spider.py
+++ fost-py/Python/Fost/Test/spider.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright 2008-2009, Felspar Co Ltd. http://fost.3.felspar.com/
+# Copyright 2008-2011, Felspar Co Ltd. http://support.felspar.com/
 # Distributed under the Boost Software License, Version 1.0.
 # See accompanying file LICENSE_1_0.txt or copy at
 #     http://www.boost.org/LICENSE_1_0.txt
@@ -7,7 +7,10 @@
 import glob
 import os
 import random
-import unittest
+try:
+    import unittest2 as unittest
+except:
+    import unittest
 import urlparse
 from Fost.internet import x_www_form_urlencoded
 from Fost.internet.useragent import agent
