2754e98a6e7a7b1b88a152dc5a4feec261a1588c
Kirit Sælensminde <k@kirit.com>
2012-08-15 07:09:52 +0000

    Merged from trunk

--- fost-py/Cpp/fost-json-py/jsonblob.cpp
+++ fost-py/Cpp/fost-json-py/jsonblob.cpp
@@ -1,5 +1,5 @@
 /*
-    Copyright 2008, Felspar Co Ltd. http://fost.3.felspar.com/
+    Copyright 2008-2012, Felspar Co Ltd. http://support.felspar.com/
     Distributed under the Boost Software License, Version 1.0.
     See accompanying file LICENSE_1_0.txt or copy at
         http://www.boost.org/LICENSE_1_0.txt
@@ -7,13 +7,14 @@
 
 
 #include "jsonblob.hpp"
+#include <fost/unicode>
 
 
 using namespace fostlib;
 
 
 nullable< string > filename( const jsondb &db ) {
-    return db.filename();
+    return coerce< nullable< string > >(db.filename());
 }
 std::auto_ptr< jsondb::local > get_local( jsondb &db ) {
     return std::auto_ptr< jsondb::local >( new jsondb::local( db, jcursor() ) );
