3287ece34c027c8b9a3b4aaf306ba519ddb7c34e
Kirit Sælensminde <k@kirit.com>
2018-07-28 13:35:50 +0700

    Initial pass at removing un-needed code.

--- Docs/fost-crypto-py.hpp
+++
@@ -1,7 +0,0 @@
-/** \defgroup fost_py_crypto Fost.crypto
-    \ingroup fost_py
-
-    \brief Fost.crypto is a thin Python wrapper over some of the Fost cryptography services.
-
-    Fost 4 uses OpenSSL to provide the basic functionality and Python already has good crypto libraries that ship as standard.
-*/
